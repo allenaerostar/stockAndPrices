cc86b660edc768eb844edc059282cede945c4664
roberto hechen <robertozeb@hotmail.com>
2020-11-23 19:52:37 -0500

    Updated testing signUp/Login for server side

--- stockAndPrices/services/app/tests/test_app.py
+++ stockAndPrices/services/app/tests/test_app.py
@@ -17,6 +17,37 @@ def test_login_page(client):
 
 # POST request to Login Page
 def test_logged_in(client):
-    res = client.post("/login", data=dict(username='Roberto'))
+    info = {"username":"username", "password":"password"}
+    res = client.post("/login", data=info)
     assert res.status_code == 200
-    assert res.data == b"<h1>Welcome Roberto!</h1>"
+    assert res.data == b"Login Successfully"
+
+def test_logged_in_failed(client):
+    info = {"username":"hahahaha", "password":"12345678"}
+    res = client.post("/login", data=info)
+    assert res.status_code == 200
+    assert res.data == b"Incorrect Username or Password!"
+
+
+# GET request to signUp Page
+def test_signUo_page(client):
+    res = client.get('/signUp')
+    assert res.status_code == 200
+    assert res.data == b'<h1>Sign Up Page</h1>'
+
+
+# POST request to signUp Page
+def test_signUp_Successful(client):
+    info = {"username":"hahahaha", "password":"12345678",
+            "email":"some@mail.com"}
+    res = client.post('/signUp', data=info)
+    assert res.status_code == 200
+    assert res.data == b"Account Created. Please Sign In"
+
+def test_signUp_failed(client):
+    info = {"username":"username", "password":"password"}
+    res = client.post('/signUp', data=info)
+    assert res.status_code == 200
+    assert res.data == b"Username already exist! Try other username"
+    
+
