88989ccd58c7db0b5af48c7d60fc248b03202532
roberto hechen <robertozeb@hotmail.com>
2021-03-01 16:24:21 -0500

    STOC43 Backend validation for login/signUp form

--- stockAndPrices/services/app/main.py
+++ stockAndPrices/services/app/main.py
@@ -3,11 +3,11 @@ from flask_cors import CORS
 from configuration.Origin import Origin
 from configuration.swagger import SWAGGER_URL, API_URL
 from util.dbUtility import getMongoDbClient
+from mongosanitizer.sanitizer import sanitize
 from flask_swagger_ui import get_swaggerui_blueprint
 from games import games_blueprint
-import logging
+import logging, re
 from configuration.Loggings import log_fname, log_fmat, log_level
-
 
 app = Flask(__name__)
 app.config.from_pyfile("configuration/Config.py")
@@ -45,6 +45,9 @@ def login():
     else:
         user = request.form.get('username')
         pwd = request.form.get('password')
+        val = validate(user,pwd,'',False)
+        if val != 'true':
+            return val, 200
 
         client = getMongoDbClient()
 
@@ -57,6 +60,7 @@ def login():
             "username": user,
             "password": pwd
         }
+        sanitize(userInfo)
         if accounts.find_one(userInfo) is None:
             return "Incorrect Username or Password!", 401
         else:
@@ -70,19 +74,24 @@ def signUp():
         user = request.form.get('username')
         pwd = request.form.get('password')
         email = request.form.get('email')
-
+        val = validate(user,pwd,email,True)
+        if val != 'true':
+            return val, 200
+        
         client = getMongoDbClient()
         if client is None:
             return "Connection Error! Internal Server Error.", 500
 
         stockAndPrices = client.stockAndPrices
         accounts = stockAndPrices.accounts
-        userInfo = {
-            "username": user,
-            "email": email
-            }
+        userInfo = {"username": user}
+        emailInfo = {"email": email}
+        sanitize(userInfo)
+        sanitize(emailInfo)
         if accounts.find_one(userInfo):
             return "Username already exist! Try another username.", 200
+        elif accounts.find_one(emailInfo):
+            return "Email already used! Try another email.", 200
         else:
             new_user = accounts.insert_one({
                 "username": user,
@@ -90,6 +99,25 @@ def signUp():
                 "email": email
             })
             return "Account Created. Please Sign In With Your Credentials.", 201
+
+
+def validate(username, password, email, signUp):
+    # username check
+    if len(username)<8 or len(username)>12:
+        return "Username should have 8 to 12 characters."
+    # passowrd check
+    if len(password)<8 or len(password)>12:
+        return "Password should have 8 to 12 characters."
+    
+    # only check password contain capital letter/number, and email if it is signUp form
+    if signUp:
+        if not (re.search('\d', password) and re.search('[A-Z]', password)):
+            return "Password should include capital letter and number"
+
+        # the regex means "Has only one @, at least one character before the @, before the period and after it"
+        if not re.search('^[^@]+@[^@]+\.[^@]+$', email):
+            return "Not a valid email address"
+    return "true"
 
 if __name__ == "__main__":
     app.run(host=app.config["HOST"], port=app.config["PORT"], debug = app.config["DEBUG"], use_reloader=False)
