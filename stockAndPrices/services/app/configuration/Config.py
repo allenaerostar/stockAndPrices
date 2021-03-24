1b5e7f89285e2fc80f83eac98eaeba879c98511e
roberto hechen <robertozeb@hotmail.com>
2021-03-24 16:09:56 -0400

    resolved merge

--- stockAndPrices/services/app/configuration/Config.py
+++ stockAndPrices/services/app/configuration/Config.py
@@ -5,6 +5,10 @@ DEBUG = True
 TESTING = True
 PORT = 5000
 HOST = "0.0.0.0"
-SECRET_KEY = 'thisisasecretkey!*~'
-DB_HOST = 'mongodb';
-DB_PORT = '27017';
+SECRET_KEY = 'ThisIsARandomSecretKey!*~'
+DB_HOST = 'mongodb'
+DB_PORT = '27017'
+
+# JWT for flask-praetorian
+JWT_ACCESS_LIFESPAN = {'hours': 24}
+JWT_REFRESH_LIFESPAN = {'days': 30}
--- stockAndPrices/services/app/main.py
+++ stockAndPrices/services/app/main.py
@@ -7,6 +7,8 @@ from flask_swagger_ui import get_swaggerui_blueprint
 from games import games_blueprint
 import logging
 from configuration.Loggings import log_fname, log_fmat, log_level
+import flask_praetorian
+
 
 
 app = Flask(__name__)
@@ -16,6 +18,10 @@ app.config.from_pyfile("configuration/Config.py")
 logging.basicConfig(filename=log_fname,
                     format=log_fmat)
 app.logger.setLevel(log_level)
+
+# Flask Praetorian / Guard #
+guard = flask_praetorian.Praetorian()
+guard.init_app(app)
 
 # CORS #
 cors = CORS(app, resources={r"/.*": {"origins": Origin}})
@@ -38,58 +44,62 @@ app.register_blueprint(games_blueprint)
 def home():
     return "<h1>Home Page</h1>"
 
-@app.route('/login', methods=['POST', 'GET'])
+@app.route('/login', methods=['POST'])
 def login():
-    if request.method == "GET":
-        return "<h1>Login Page</h1>"
+    user = request.form.get('username')
+    pwd = request.form.get('password')
+    val = validate(user,pwd,'',False)
+    if val != 'true':
+        return val, 200
+
+    client = getMongoDbClient()
+
+    if client is None:
+        return "Connection Error! Internal Server Error.", 500
+
+    stockAndPrices = client.stockAndPrices
+    accounts = stockAndPrices.accounts
+    userInfo = {
+        "username": user,
+        "password": pwd
+    }
+    sanitize(userInfo)
+    if accounts.find_one(userInfo) is None:
+        return "Incorrect Username or Password!", 401
     else:
-        user = request.form.get('username')
-        pwd = request.form.get('password')
+        return "Login Successfully", 200
 
-        client = getMongoDbClient()
-
-        if client is None:
-            return "Connection Error! Internal Server Error.", 500
-
-        stockAndPrices = client.stockAndPrices
-        accounts = stockAndPrices.accounts
-        userInfo = {
-            "username": user,
-            "password": pwd
-        }
-        if accounts.find_one(userInfo) is None:
-            return "Incorrect Username or Password!", 401
-        else:
-            return "Login Successfully", 200
-
-@app.route('/signUp', methods=['POST', 'GET'])
+@app.route('/signUp', methods=['POST'])
 def signUp():
-    if request.method == "GET":
-        return "<h1>Sign Up Page</h1>"
+    user = request.form.get('username')
+    pwd = request.form.get('password')
+    email = request.form.get('email')
+    val = validate(user,pwd,email,True)
+    if val != 'true':
+        return val, 200
+    
+    client = getMongoDbClient()
+    if client is None:
+        return "Connection Error! Internal Server Error.", 500
+
+    stockAndPrices = client.stockAndPrices
+    accounts = stockAndPrices.accounts
+    userInfo = {"username": user}
+    emailInfo = {"email": email}
+    sanitize(userInfo)
+    sanitize(emailInfo)
+    if accounts.find_one(userInfo):
+        return "Username already exist! Try another username.", 200
+    elif accounts.find_one(emailInfo):
+        return "Email already exist! Try another email.", 200
     else:
-        user = request.form.get('username')
-        pwd = request.form.get('password')
-        email = request.form.get('email')
-
-        client = getMongoDbClient()
-        if client is None:
-            return "Connection Error! Internal Server Error.", 500
-
-        stockAndPrices = client.stockAndPrices
-        accounts = stockAndPrices.accounts
-        userInfo = {
+        new_user = accounts.insert_one({
             "username": user,
+            "password": pwd,
             "email": email
-            }
-        if accounts.find_one(userInfo):
-            return "Username already exist! Try another username.", 200
-        else:
-            new_user = accounts.insert_one({
-                "username": user,
-                "password": pwd,
-                "email": email
-            })
-            return "Account Created. Please Sign In With Your Credentials.", 201
+        })
+        return "Account Created. Please Sign In With Your Credentials.", 201
+
 
 if __name__ == "__main__":
     app.run(host=app.config["HOST"], port=app.config["PORT"], debug = app.config["DEBUG"], use_reloader=False)
